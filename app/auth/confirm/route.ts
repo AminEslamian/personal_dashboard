34ccb8bfc2e3f3c83dc969dbf7fc55e5964dc27d
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2026-07-02 11:38:59 +0330

    fix: correc supabase EmailOtpType import typo

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,4 +1,4 @@
-import { type EmailOtpsType } from '@supabase/supabase-js'
+import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest } from 'next/server'
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
@@ -6,7 +6,7 @@ import { createClient } from '@/utils/supabase/server'
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
-  const type = searchParams.get('type') as EmailOtpsType | null
+  const type = searchParams.get('type') as EmailOtpType | null
   const next = searchParams.get('next') ?? '/'
 
   if (token_hash && type) {
